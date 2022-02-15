d30b4cbc525ee34e804ec8364781a026b79694ff
brutusd01 <derose_brutus@hotmail.com>
2022-02-15 18:11:30 -0500

    Add gameplay variables to PlayerController script

--- Project Artillery/Assets/_Scripts/Player/PlayerController.cs
+++ Project Artillery/Assets/_Scripts/Player/PlayerController.cs
@@ -1,13 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private InputManager _inputManager;
+    public Rigidbody rb;
+    public float walkSpeed = 10f;
+    public float jumpHeight = 5f;
+    public Transform gameCamera;
+    public Vector2 moveInput;
+    public Vector3 moveVector;
     void Start()
     {
-        
+        if(rb == null)rb = gameObject.GetComponent<Rigidbody>();
+        gameCamera = Camera.main.transform;
     }
 
     // Update is called once per frame
