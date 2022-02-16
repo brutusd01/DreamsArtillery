b621dc62e01dd62fa3d71eaf3e849b7f78f93e6e
brutusd01 <derose_brutus@hotmail.com>
2022-02-15 21:47:50 -0500

    Add movement to PlayerController

--- Project Artillery/Assets/_Scripts/Player/PlayerController.cs
+++ Project Artillery/Assets/_Scripts/Player/PlayerController.cs
@@ -9,16 +9,68 @@ public class PlayerController : MonoBehaviour
     public float jumpHeight = 5f;
     public Transform gameCamera;
     public Vector2 moveInput;
+    public bool jumpInput;
     public Vector3 moveVector;
-    void Start()
+    Vector3 currentVelocity;
+
+    void Awake()
     {
         if(rb == null)rb = gameObject.GetComponent<Rigidbody>();
-        gameCamera = Camera.main.transform;
     }
 
-    // Update is called once per frame
+    void OnEnable()
+    {
+        _inputManager.moveEvent += onMove;
+        _inputManager.jumpEvent += onJump;
+        _inputManager.jumpCanceledEvent += onJumpEnd;
+        _inputManager.attack1Event += onFire1;
+    }
+
+    void OnDisable()
+    {
+        _inputManager.moveEvent -= onMove;
+        _inputManager.jumpEvent -= onJump;
+        _inputManager.jumpCanceledEvent -= onJumpEnd;
+        _inputManager.attack1Event -= onFire1;
+    }
+    void onMove(Vector2 value)
+    {
+        moveInput = value;
+    }
+
+    void onJump()
+    {
+        jumpInput = true;
+    }
+
+    void onJumpEnd()
+    {
+        jumpInput = false;
+    }
+
+    void onFire1()
+    {
+        //Shoot the gun!
+        //weapon.doFire(); or something 
+    }
+
+    void CalculateMovement()
+    {
+        Vector3 camForward = gameCamera.forward;
+        Vector3 camRight = gameCamera.right;
+
+        camRight.y = camForward.y = 0;
+        Vector3 moveDir = (camRight.normalized * moveInput.x) + (camForward.normalized * moveInput.y);
+        Vector3 targetSpeed = moveDir * walkSpeed;
+        moveVector = Vector3.SmoothDamp(moveVector, targetSpeed, ref currentVelocity, .15f);
+        print(moveInput);
+    }
     void Update()
     {
-        
+        CalculateMovement();
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + transform.TransformDirection(moveVector) * Time.fixedDeltaTime);
     }
 }
